ca845bc83475c9d715948fceb09ae43461d062fd
Tab-Y <tlyoumans@gmail.com>
2022-10-08 13:06:07 -0400

    hardcoded html

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,6 @@
+
+
+
 var datetime = null,
         date = null;
 
@@ -5,12 +8,21 @@ var update = function () {
     date = moment(new Date())
     datetime.html(date.format('dddd, MMMM Do YYYY'));
 };
-
+// targets id currentDay to put a timestamp
 $(document).ready(function(){
     datetime = $('#currentDay')
     update();
     setInterval(update, 1000);
 });
-function sendForm(){
-    
+
+// set time classes to change with time using jQuery
+// target id compared to time
+// reset at 0000 (midnight)
+function checkTime(){
+
+}
+
+// function to save textarea input to local storage based on id
+function saveTextarea(){
+
 }
